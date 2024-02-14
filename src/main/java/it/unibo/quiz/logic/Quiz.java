9f2174760f30ed248fe7f25caa979d4e547e08a8
Nax <federico.diotallevi@studio.unibo.it>
2024-02-14 17:05:17 +0100

    [API] added methods to request given answers and substituted add and remove answer with set new answers.

--- src/main/java/it/unibo/quiz/logic/Quiz.java
+++ src/main/java/it/unibo/quiz/logic/Quiz.java
@@ -1,14 +1,22 @@
 package it.unibo.quiz.logic;
 
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
 import it.unibo.quiz.questions.Question;
 
 public interface Quiz {
 
-    boolean addAnswer(Question question, String givenAnswer);
-
-    boolean removeAnswer(Question question, String givenAnswer);
+    boolean setNewAnswers(Question question, Collection<? extends String> givenAnswers);
 
     Question getNext();
     
     Question getPrevious();
+
+    Map<Question, Set<String>> getGivenAnswers();
+
+    Set<String> getGivenAnswers(Question question);
+
+    void reset();
 }
